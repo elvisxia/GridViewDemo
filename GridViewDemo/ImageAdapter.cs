cb83439114c1605f23df70225fa5703ccb373a4e
v-xucxia <v-xucxia@PC-ELVIS>
2017-02-16 10:57:38 +0800

    Add project files.

---
+++ GridViewDemo/ImageAdapter.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Java.Lang;
+
+namespace GridViewDemo
+{
+    public class ImageAdapter : BaseAdapter
+    {
+        private readonly Context context;
+
+        public List<int> thumbIds = new List<int> {
+            Resource.Drawable.sample_0,
+            Resource.Drawable.sample_1,
+            Resource.Drawable.sample_2,
+            Resource.Drawable.sample_3,
+            Resource.Drawable.sample_4,
+            Resource.Drawable.sample_5,
+            Resource.Drawable.sample_6,
+            Resource.Drawable.sample_7,
+
+        };
+        public ImageAdapter(Context context)
+        {
+            this.context = context;
+        }
+        public override int Count
+        {
+            get
+            {
+                return thumbIds.Count;
+            }
+        }
+
+        public override Java.Lang.Object GetItem(int position)
+        {
+            return null;
+        }
+
+        public override long GetItemId(int position)
+        {
+            return 0;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            ImageView imageView;
+
+            if (convertView == null)
+            {
+                imageView = new ImageView(context);
+                imageView.LayoutParameters = new AbsListView.LayoutParams(85, 85);
+                imageView.SetScaleType(ImageView.ScaleType.CenterCrop);
+                imageView.SetPadding(8, 8, 8, 8);
+            }
+            else
+            {
+                imageView = (ImageView)convertView;
+            }
+            imageView.SetImageResource(thumbIds[position]);
+            return imageView;
+        }
+    }
+}
---
+++ GridViewDemo/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace GridViewDemo
+{
+    [Activity(Label = "GridViewDemo", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        GridView gridview;
+        ImageAdapter mGridViewAdapter;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+            gridview = FindViewById<GridView>(Resource.Id.gridview);
+            mGridViewAdapter= new ImageAdapter(this);
+            gridview.Adapter = mGridViewAdapter;
+            Button btnClick = FindViewById<Button>(Resource.Id.btnClick);
+            btnClick.Click += BtnClick_Click;
+
+        }
+
+        private void BtnClick_Click(object sender, System.EventArgs e)
+        {
+            int newSpan = 4; // new columns count
+
+            gridview.SetNumColumns(newSpan); // set columns
+            mGridViewAdapter.NotifyDataSetChanged(); // notify custom adapter
+            mGridViewAdapter.thumbIds[0] = Resource.Drawable.gluehbirne;
+            mGridViewAdapter.thumbIds[1] = Resource.Drawable.gluehbirne;
+            mGridViewAdapter.thumbIds[2] = Resource.Drawable.gluehbirne;
+            mGridViewAdapter.thumbIds[3] = Resource.Drawable.gluehbirne;
+            mGridViewAdapter.thumbIds[4] = Resource.Drawable.gluehbirne;
+        }
+    }
+}
+
